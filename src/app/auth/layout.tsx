7d027abb083eab8903cc60a57ed8c19f64f3503c
GilTorch <infotorch2014@gmail.com>
2025-01-12 19:34:13 -0400

    Create signup UI

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,5 +1,6 @@
 
 import SideNav from '@/app/ui/auth/sidenav';
+import Copyright from '../ui/Copyright';
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -7,7 +8,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="w-full hidden md:block md:w-1/2">
           <SideNav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12 mt-36">{children}</div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12 mt-36">
+        {children}
+        <footer className="mt-8 flex justify-center items-center">
+            <Copyright />
+        </footer>
+      </div>
     </div>
   );
 } 
---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,44 @@
+import Input from "@/app/ui/auth/input";
+import LineWithText from "@/app/ui/auth/LineWithText";
+import MainHeading from "@/app/ui/auth/main-heading";
+import Link from "next/link";
+import SocialButton from "@/app/ui/auth/SocialButton";
+
+export default async function Page() {
+
+    return <div>
+        <MainHeading>KREYE ON KONT</MainHeading> 
+        <div className="mt-20">
+            <div className="mt-8">
+                <div className="flex flex-col md:flex-row gap-8 md:gap-3">
+                    <Input type="text" value="" placeholder="Non" alt="Email input" />
+                    <Input type="text" value="" placeholder="Prenon" alt="Email input" />
+                </div>
+            </div>
+            <div className="mt-8">
+                <Input type="email" value="" placeholder="Imel" alt="Email input" />
+            </div>
+            <div className="mt-8" >
+                <Input type="password" value="" placeholder="Modpas" alt="Password input" />
+            </div>
+            <div className="mt-8" >
+                <Input type="password" value="" placeholder="Konfime modpas" alt="Password input" />
+            </div>
+        </div>
+        <div className="mt-8 flex flex-col gap-3">
+            <Link href={'/auth/forgot-password'} className="text-headerColor font-bodyFont underline">
+                Mwen bliye modpas mwen
+            </Link>
+            <Link href={'/auth/login'} className="text-headerColor font-bodyFont underline">
+               Ou gen yon kont? Konekte
+            </Link>
+        </div>
+        <div className="mt-20 mb-20">
+            <LineWithText />
+        </div>
+        <div className="flex flex-col md:flex-row items-center justify-between md:gap-3 gap-8">
+            <SocialButton src="/svg/google.svg" text="Konekte ak Google" />
+            <SocialButton src="/svg/facebook.svg" text="Konekte ak Facebook" />
+        </div>
+    </div>
+}
